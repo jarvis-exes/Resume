ddd82232f3c04203de82277d07c579d23cb597bb
Shubham Sharma <8696771553shubham@gmail.com>
2024-07-13 19:59:59 +0530

    removed script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openMenu() {
-  var x = document.getElementById("burgermenu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
