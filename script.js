567b638406e62ad3e6f6ea8f5ca9c2ccae2ddcaa
Shubham Sharma <8696771553shubham@gmail.com>
2024-07-13 19:48:20 +0530

    Added new menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  var x = document.getElementById("burgermenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
